bcf17e1f4085f27eb3022c94357046ef1128da6a
Patrick Lynch <patrick.lynch@unipart.com>
2020-07-07 17:26:16 +0100

    uses last entries timestamps now

--- models/trigger.py
+++ models/trigger.py
@@ -1,10 +1,19 @@
 from core.models import trigger
-import feedparser
+import feedparser, datetime, time
 
 class _rss(trigger._trigger):
     feed=str()
     feed_name=str()
+    lastEntryTimestamp = int()
+    onlyNew = bool()
 
     def check(self):
         rssFeed = feedparser.parse(self.feed)
-        print(rssFeed.entries)
+        for entry in rssFeed["entries"]:
+            if self.onlyNew:
+                if time.mktime(entry["published_parsed"]) > self.lastEntryTimestamp:
+                    self.result["events"].append(entry)
+            else:
+                self.result["events"].append(entry)
+        self.lastEntryTimestamp = datetime.datetime.now().timestamp()
+        self.update(["lastEntryTimestamp"])
